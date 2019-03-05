b966cbb6a89fc9d96fa2ce43878176042b7d9466
pablo-souza <47615568+pablo-souza@users.noreply.github.com>
2019-03-05 09:07:36 -0300

    classes Pessoas ,Usuarios e Endereco
    alguém poderia fazer a classe model de usuários?

---
+++ AgendamentoBanhoTosa/src/main/java/br/com/agendamento/main/Endereco.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agendamento.main;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Endereco {
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String CEP;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+    
+}
---
+++ AgendamentoBanhoTosa/src/main/java/br/com/agendamento/main/Usuarios.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agendamento.main;
+package br.com.agendamento.util;
+
+/**
+ *classe de usuario, data da modificação 05/03/2019
+ * @author Pablo
+ */
+public class Usuarios extends Pessoa{
+    private int nivel;
+    private String nomeUsuario;
+    private String RG;
+    private String senha;
+    //endereco
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+}
