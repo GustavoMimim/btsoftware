4b70c656acbb098b185d326808c45e12fb37744d
João Marcos <joao.santos160@fatec.sp.gov.br>
2019-03-12 00:09:37 -0300

    Adição da ServicoDAO e seu teste

---
+++ AgendamentoBanhoTosa/src/main/java/br/com/agendamento/dao/ServicoDAO.java
@@ -0,0 +1,7 @@
+package br.com.agendamento.dao;
+
+import br.com.agendamento.domain.Servico;
+
+public class ServicoDAO extends GenericDAO<Servico> {
+
+}
---
+++ AgendamentoBanhoTosa/src/test/java/br/com/agendamento/dao/ServicoDAOTest.java
@@ -0,0 +1,25 @@
+package br.com.agendamento.dao;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.agendamento.domain.Servico;
+
+public class ServicoDAOTest {
+
+	@Ignore
+	@Test
+	public void salvar() {
+		// Instanciando objeto
+		Servico servico = new Servico();
+
+		// Atribuindo as coisas ao objeto
+		servico.setDescricao("Tosa Geral");
+		servico.setPreco(40);
+		servico.setTipoServico("Tosa");
+
+		// Instanciando para salvar
+		ServicoDAO servicoDAO = new ServicoDAO();
+		servicoDAO.salvar(servico);
+	}
+}
