5bd9a397a3ef66dbe8932eb427e6ff87ae9d9658
locrz <lucas.oliveira.timburi@gmail.com>
2019-03-18 21:46:37 -0300

    Controller Usuários

---
+++ AgendamentoBanhoTosa/src/main/java/br/com/agendamento/bean/UsuarioBean.java
@@ -0,0 +1,11 @@
+package br.com.agendamento.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UsuarioBean {
+
+	public void salvar() {
+		System.out.println("teste ");
+	}
+}
